6aba41eab861dc10926cb738fce10b693751fca5
Christopher Betancourt <159607929+Chrisbetann@users.noreply.github.com>
2024-10-04 11:22:40 -0400

    Update monitor.py
    Added CAPTCHA Handling
    Modified Notification Logic
    Removed Unnecessary Variables

--- monitors/nike/monitor.py
+++ monitors/nike/monitor.py
@@ -1,65 +1,44 @@
-from random_user_agent.params import SoftwareName, HardwareType
-from random_user_agent.user_agent import UserAgent
+# monitor.py
 
 import requests as rq
 import urllib3
 from fp.fp import FreeProxy
+from random_user_agent.user_agent import UserAgent
 
 from datetime import datetime
 import time
-
 import json
 import logging
 import traceback
 
 import locations
-from config import WEBHOOK, LOCATION, LANGUAGE, ENABLE_FREE_PROXY, FREE_PROXY_LOCATION, DELAY, PROXY, KEYWORDS, USERNAME, AVATAR_URL, COLOUR
+from config import (
+    LOCATION, LANGUAGE, ENABLE_FREE_PROXY, FREE_PROXY_LOCATION, DELAY, PROXY,
+    KEYWORDS, software_names, hardware_type, send_sms_via_textlocal
+)
 
+# Initialize logging
+logging.basicConfig(
+    filename='nike-monitor.log',
+    filemode='a',
+    format='%(asctime)s - %(name)s - %(message)s',
+    level=logging.DEBUG
+)
 
-logging.basicConfig(filename='nike-monitor.log', filemode='a', format='%(asctime)s - %(name)s - %(message)s', level=logging.DEBUG)
+# Setting up Random User Agent
+user_agent_rotator = UserAgent(software_names=software_names, hardware_types=hardware_type)
 
-software_names = [SoftwareName.CHROME.value]
-hardware_type = [HardwareType.MOBILE__PHONE]
-user_agent_rotator = UserAgent(software_names=software_names, hardware_type=hardware_type)
-
-if ENABLE_FREE_PROXY:  
+# Initialize Free Proxy if enabled
+if ENABLE_FREE_PROXY:
     proxy_obj = FreeProxy(country_id=FREE_PROXY_LOCATION, rand=True)
 
-
 INSTOCK = []
-
-def discord_webhook(title, colour, url, thumbnail, price, style_code):
-    """
-    Sends a Discord webhook notification to the specified webhook URL
-    """
-    data = {
-        'username': USERNAME,
-        'avatar_url':  AVATAR_URL,
-        'embeds': [{
-            'title': title,
-            'url': url,
-            'thumbnail': {'url': thumbnail},
-            'color': int(COLOUR),
-            'footer': {'text': 'Developed by GitHub:yasserqureshi1'},
-            'timestamp': str(datetime.utcnow()),
-            'fields': [
-                {'name': 'Colour', 'value': colour},
-                {'name': 'Price', 'value': price},
-                {'name': 'Style Code', 'value': style_code},
-            ]
-        }]
-    }
-    
-    result = rq.post(WEBHOOK, data=json.dumps(data), headers={"Content-Type": "application/json"})
-
-    try:
-        result.raise_for_status()
-    except rq.exceptions.HTTPError as err:
-        logging.error(msg=err)
-    else:
-        print("Payload delivered successfully, code {}.".format(result.status_code))
-        logging.info(msg="Payload delivered successfully, code {}.".format(result.status_code))
-
+___standard_api___ = [
+    'GB', 'US', 'AU', 'AT', 'BE', 'BG', 'CA', 'CN', 'HR', 'CZ', 'DK', 'EG',
+    'FI', 'FR', 'DE', 'HU', 'IN', 'ID', 'IE', 'IT', 'MY', 'MX', 'MA', 'NL',
+    'NZ', 'NO', 'PH', 'PL', 'PT', 'PR', 'RO', 'RU', 'SA', 'SG', 'SI', 'ZA',
+    'ES', 'SE', 'CH', 'TR', 'AE', 'VN', 'JP'
+]
 
 def monitor():
     """
@@ -73,29 +52,27 @@ def monitor():
     # Ensures that first scrape does not notify all products
     start = 1
 
-    # Initialising proxy and headers
+    # Initialize proxy and headers
     if ENABLE_FREE_PROXY:
         proxy = {'http': proxy_obj.get()}
     elif PROXY != []:
         proxy_no = 0
-        proxy = {} if PROXY == [] else {"http": PROXY[proxy_no], "https": PROXY[proxy_no]}
+        proxy = {"http": PROXY[proxy_no], "https": PROXY[proxy_no]}
     else:
         proxy = {}
     user_agent = user_agent_rotator.get_random_user_agent()
 
     while True:
-        # Makes request to site and stores products 
+        # Makes request to site and stores products
         try:
-            if LOCATION in locations.___standard_api___:
-                to_discord = locations.standard_api(INSTOCK, LOCATION, LANGUAGE, user_agent, proxy, KEYWORDS, start)
-            
+            if LOCATION in ___standard_api___:
+                locations.standard_api(INSTOCK, LOCATION, LANGUAGE, user_agent, proxy, KEYWORDS, start)
             else:
                 print(f'LOCATION "{LOCATION}" CURRENTLY NOT AVAILABLE. IF YOU BELIEVE THIS IS A MISTAKE PLEASE CREATE AN ISSUE ON GITHUB OR MESSAGE THE #issues CHANNEL IN DISCORD.')
                 return
-            
-            for product in to_discord:
-                discord_webhook(product['title'], product['colour'], product['url'], product['thumbnail'], product['price'], product['style_code'])
-                print(product['title'])
+
+            # Allows changes to be notified
+            start = 0
 
         except rq.exceptions.RequestException as e:
             logging.error(e)
@@ -103,7 +80,7 @@ def monitor():
 
             # Rotates headers
             user_agent = user_agent_rotator.get_random_user_agent()
-            
+
             if ENABLE_FREE_PROXY:
                 proxy = {'http': proxy_obj.get()}
 
@@ -114,9 +91,6 @@ def monitor():
         except Exception as e:
             print(f"Exception found: {traceback.format_exc()}")
             logging.error(e)
-
-        # Allows changes to be notified
-        start = 0
 
         # User set delay
         time.sleep(float(DELAY))
